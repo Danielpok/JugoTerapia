fab1ee50a05ab562f5e410289831c1c50ab83cef
Jorge Jonathan Moreno Zúñiga <JorgeJonathanMorenoZ@gmail.com>
2015-11-23 21:51:47 -0700

    Cleanup server code

--- server.js
+++ server.js
@@ -9,21 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, "www/index.html"));
 });
 
-// accept POST request on the homepage
-app.post('/', function (req, res) {
-  res.send('Got a POST request');
-});
-
-// accept PUT request at /user
-app.put('/user', function (req, res) {
-  res.send('Got a PUT request at /user');
-});
-
-// accept DELETE request at /user
-app.delete('/user', function (req, res) {
-  res.send('Got a DELETE request at /user');
-});
-
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
